0dbfd3cb1e1315f8315db7f637bc09a2b40b498c
André Abadesso <andre.abadesso@gmail.com>
2015-11-22 16:06:06 -0200

    feat(): improved player layout and replaced querystring logic

--- client/app/player.js
+++ client/app/player.js
@@ -3,28 +3,6 @@
 
     $(document).ready(function() {
         var client;
-        var QueryString = function() {
-            // This function is anonymous, is executed immediately and 
-            // the return value is assigned to QueryString!
-            var query_string = {};
-            var query = window.location.search.substring(1);
-            var vars = query.split("&");
-            for (var i = 0; i < vars.length; i++) {
-                var pair = vars[i].split("=");
-                // If first entry with this name
-                if (typeof query_string[pair[0]] === "undefined") {
-                    query_string[pair[0]] = decodeURIComponent(pair[1]);
-                    // If second entry with this name
-                } else if (typeof query_string[pair[0]] === "string") {
-                    var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
-                    query_string[pair[0]] = arr;
-                    // If third or later entry with this name
-                } else {
-                    query_string[pair[0]].push(decodeURIComponent(pair[1]));
-                }
-            }
-            return query_string;
-        }();
 
         function _generateCss(data) {
             var string =
@@ -61,14 +39,15 @@
             };
         }
 
-        if (QueryString.url) {
+        var url = window.location.search.replace('?url=', '');
+        if (url) {
             $.ajax({
                 type: 'POST',
                 url: '/get_stream',
                 processData: false,
                 contentType: 'application/json',
                 data: JSON.stringify({
-                    "streamUrl": QueryString.url
+                    "streamUrl": url
                 }),
                 success: function(r) {
                     playVideo(r.proxy);
