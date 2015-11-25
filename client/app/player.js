08466032ac7beae4841f461a49679f4ff9f32978
André Abadesso <andre.abadesso@gmail.com>
2015-11-25 00:15:35 -0200

    feat(player): added debounced check to reconnect if the player loses connection, closes #6

--- client/app/player.js
+++ client/app/player.js
@@ -16,6 +16,23 @@
         }.bind(this));
     };
 
+    // Taken from Underscore.js
+    Player.prototype._debounce = function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
     Player.prototype._jsonRequest = function(url, body, callback) {
         var xhr = new XMLHttpRequest();
         xhr.open('POST', url, true);
@@ -28,9 +45,7 @@
     };
 
     Player.prototype._generateCss = function(data) {
-        var string =
-            'transparent url(data:image/jpeg;base64,' +
-            data;
+        var string = 'transparent url(data:image/jpeg;base64,' + data;
         string += ') top left / 100% 100% no-repeat';
 
         return string;
@@ -44,6 +59,32 @@
         image.src = url;
     };
 
+    Player.prototype._updateImage = function(image) {
+        this._isValidImageUrl('data:image/jpeg;base64,' + image, function(err) {
+
+            if (err) {
+                console.log('Invalid image.');
+                return;
+            }
+
+            /*
+             * Updates the div background image to display images if it was a
+             * video.
+             */
+            this.player.style.background = this._generateCss(image);
+
+            /*
+             * Debounced call to reconnect to request the stream again
+             * every 5 seconds.
+             */
+            this._debounce(function() {
+                console.log('Retrying connection.');
+                this.init();
+            }.bind(this), 5000);
+
+        }.bind(this));
+    };
+
     Player.prototype._playVideo = function(url) {
         if (this.client && this.client.onmessage) {
             this.client.onmessage = null;
@@ -51,17 +92,8 @@
         }
 
         this.client = new WebSocket(url);
-        this.client.onmessage = function(data) {
-            this._isValidImageUrl('data:image/jpeg;base64,' + data.data, function(err) {
-
-                if (err) {
-                    console.log('Invalid image.');
-                    return;
-                }
-
-                this.player.style.background = this._generateCss(data.data);
-
-            }.bind(this));
+        this.client.onmessage = function(res) {
+            this._updateImage(res.data);
         }.bind(this);
     };
 
